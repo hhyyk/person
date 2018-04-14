bb68da94531d54882cab4976cf4b6bc1aad9710e
hhyyk <1078530857@qq.com>
2018-04-14 08:46:31 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,26 @@
+/*
+* @Author: asus
+* @Date:   2018-04-12 10:59:35
+* @Last Modified by:   asus
+* @Last Modified time: 2018-04-13 08:25:49
+*/
+var button1=document.querySelector(".about1");
+    button1.onclick=function(){
+    	alert("主人名称：贺慧")
+}
+var button2=document.querySelector(".about2");
+    button2.onclick=function(){
+    	alert("主人性别：女")
+}
+var button3=document.querySelector(".about3");
+    button3.onclick=function(){
+    	alert("专业：计算机专业")
+}
+var button4=document.querySelector(".about4");
+    button4.onclick=function(){
+    	alert("联系电话：+012 345 6789")
+}
+var button5=document.querySelector(".about5");
+    button5.onclick=function(){
+    	alert("Email：mail@example.com")
+}
